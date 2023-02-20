82321cdf182db4e8e132a4af2e9aa76e5c1e0e4b
sunderiyab <hairiima888@gmail.com>
2023-02-19 22:48:35 -0800

    pypoll added

--- pypoll/main.py
+++ pypoll/main.py
@@ -8,7 +8,7 @@ with open (csvpath, "r" ) as csvfile:
     csv_header = next(csvreader)
     #check to see if im opening the right file
     print(f"header: {csv_header}")
-    
+
 #The total number of votes cast
 #init number of votes to integar value = 0
     number_of_votes = [] #empty list to store the votes
@@ -17,12 +17,46 @@ with open (csvpath, "r" ) as csvfile:
         number_of_votes.append("vote")
     total_number = len(number_of_votes)
     print(f"total number of votes: {total_number}")
-   
+
 #A complete list of candidates who received votes
 
+    csvfile.seek(0) #pointing to the back to beginning
+    next(csvreader)
+    votes = {}
+    for row in csvreader:
+       candidate = row[2]
+       if candidate not in votes:
+        votes[candidate] = 0 
+        votes[candidate] += 1
+    for candidate, count in votes.items():
+        print(f"{candidate} : {count}")
 #The percentage of votes each candidate won
+    
+
 
 #The total number of votes each candidate won
+    csvfile.seek(0) #pointing to the back to beginning
+    next(csvreader)
+    number_of_votes = {
+    "Charles Casper Stockham" : 0,
+    "Diana DeGette": 0,
+    "Raymon Anthony Doane" : 0 
+    }
+    for row in csvreader:
+        candidates = row[2]
+        if candidates == "Charles Casper Stockham":
+            number_of_votes["Charles Casper Stockham"] += 1
+        elif candidates == "Diana DeGette":
+            number_of_votes["Diana DeGette"] +=1
+        elif candidates == "Raymon Anthony Doane":
+            number_of_votes["Raymon Anthony Doane"] +=1
+    total_votes = sum(number_of_votes.values())
+    print(f"Charles Casper Stockham: {number_of_votes['Charles Casper Stockham']}")
+    print(f"Diana DeGette: {number_of_votes['Diana DeGette']}")
+    print(f"Raymon Anthony Doane: {number_of_votes['Raymon Anthony Doane']}")
+    
 
-#The winner of the election based on popular vote
+        
+    
+
 
