a5022b917759046fff565c41d22ba18e07e0f660
sunderiyab <hairiima888@gmail.com>
2023-02-21 22:05:09 -0800

    pypoll fixed

--- pypoll/main.py
+++ pypoll/main.py
@@ -30,11 +30,8 @@ with open (csvpath, "r" ) as csvfile:
         votes[candidate] += 1
     for candidate, count in votes.items():
         print(f"{candidate} : {count}")
-#The percentage of votes each candidate won
-    
 
-
-#The total number of votes each candidate won
+  #The total number of votes each candidate won
     csvfile.seek(0) #pointing to the back to beginning
     next(csvreader)
     number_of_votes = {
@@ -50,13 +47,28 @@ with open (csvpath, "r" ) as csvfile:
             number_of_votes["Diana DeGette"] +=1
         elif candidates == "Raymon Anthony Doane":
             number_of_votes["Raymon Anthony Doane"] +=1
+
     total_votes = sum(number_of_votes.values())
+
     print(f"Charles Casper Stockham: {number_of_votes['Charles Casper Stockham']}")
     print(f"Diana DeGette: {number_of_votes['Diana DeGette']}")
     print(f"Raymon Anthony Doane: {number_of_votes['Raymon Anthony Doane']}")
+    #percentage of vote
+    for candidates, count in number_of_votes.items():
+        percentage =round(count/total_votes * 100, 3)
+        print(f"{candidates} : {percentage}%")
     
+    names = ["Charles Casper Stockham" ,"Diana DeGette" , "Raymon Anthony Doane"]
+    def winner(votes): #finding the winner 
+      vote_counts = [votes[name] for name in names]
+      max_vote_count = max(vote_counts) # highest vote counts 
+      winners =[name for name , count in votes.items() if count == max_vote_count]
+      winner_name = winners [0]
+      print(f"winner is: {winner_name} with {max_vote_count}")
+    winner(votes)
 
-        
-    
 
 
+with open("pypoll_analysis.txt", "w") as file:
+    file.write(f"total_votes: {total_votes}\n")
+    file.write(f"candidates: ${number_of_votes}\n")
