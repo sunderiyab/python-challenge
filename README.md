50c2449aa70532fa1e33cbd23a7d00f6241c2cde
sunderiyab <hairiima888@gmail.com>
2023-02-26 13:16:00 -0800

    found the winner

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # python-challenge
+On this challenge i worked with multiple csv that contains lots of datasets and one of the dataset is 'election csv'
+i needed to figure out the total votes of the election results and % of the votes each candidate won and total number of votes and lastly had to state the winner of the election
+''' python
+names = ["Charles Casper Stockham" ,"Diana DeGette" , "Raymon Anthony Doane"]
+    def winner(votes): #finding the winner 
+      vote_counts = [votes[name] for name in names]
+      max_vote_count = max(vote_counts) # highest vote counts 
+      winners =[name for name , count in votes.items() if count == max_vote_count]
+      winner_name = winners [0]
+      print(f"winner is: {winner_name} with {max_vote_count}")
+    winner(votes)
+'''
+    with this code i tried finding the winner of this election which clearly Diana DeGette with 272892 of total vote but somehow its coming out winner as Charles with 1 vote. i think My logic is right but i'm missing something or i'm calculating wrong. 
--- pypoll/main.py
+++ pypoll/main.py
@@ -17,20 +17,6 @@ with open (csvpath, "r" ) as csvfile:
         number_of_votes.append("vote")
     total_number = len(number_of_votes)
     print(f"total number of votes: {total_number}")
-
-#A complete list of candidates who received votes
-
-    csvfile.seek(0) #pointing to the back to beginning
-    next(csvreader)
-    votes = {}
-    for row in csvreader:
-       candidate = row[2]
-       if candidate not in votes:
-        votes[candidate] = 0 
-        votes[candidate] += 1
-    for candidate, count in votes.items():
-        print(f"{candidate} : {count}")
-
   #The total number of votes each candidate won
     csvfile.seek(0) #pointing to the back to beginning
     next(csvreader)
@@ -57,16 +43,18 @@ with open (csvpath, "r" ) as csvfile:
     for candidates, count in number_of_votes.items():
         percentage =round(count/total_votes * 100, 3)
         print(f"{candidates} : {percentage}%")
-    
-    names = ["Charles Casper Stockham" ,"Diana DeGette" , "Raymon Anthony Doane"]
-    def winner(votes): #finding the winner 
-      vote_counts = [votes[name] for name in names]
-      max_vote_count = max(vote_counts) # highest vote counts 
-      winners =[name for name , count in votes.items() if count == max_vote_count]
-      winner_name = winners [0]
-      print(f"winner is: {winner_name} with {max_vote_count}")
-    winner(votes)
 
+        #winner
+    csvfile.seek(0) #pointing to the back to beginning
+    next(csvreader)
+    votes = {}
+    for row in csvreader:
+        candidate = row[2]
+        if candidate not in votes:
+           votes[candidate] = 0 
+        votes[candidate] += 1
+        winner = max(votes, key=votes.get)
+    print(f"Winner: {winner}") 
 
 
 with open("pypoll_analysis.txt", "w") as file:
